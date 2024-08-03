a522c5cc3ffc8554da070a507a9e37b28241ea29
Aurorah Harmony <aurorahHarmony@gmail.com>
2024-08-04 00:02:43 +1200

    Display modal

---
+++ src/commands/utility/verify.ts
@@ -0,0 +1,42 @@
+import {
+  SlashCommandBuilder,
+  CommandInteraction,
+  PermissionFlagsBits,
+  GuildMember,
+  ModalBuilder,
+  TextInputBuilder,
+  TextInputStyle,
+  ActionRowBuilder,
+  ModalActionRowComponentBuilder,
+} from "discord.js";
+import { Command } from "../../types/Command";
+
+const verifyCommand: Command = {
+  data: new SlashCommandBuilder()
+    .setName("verify")
+    .setDescription("Brings up the welcome form for you to verify your account")
+    .setDMPermission(false),
+  async execute(interaction: CommandInteraction) {
+    const modal = new ModalBuilder()
+      .setCustomId("verifyModal")
+      .setTitle("Verification");
+
+    const ageInput = new TextInputBuilder()
+      .setCustomId("ageInput")
+      .setLabel("What's your birth date? (Month/Day/Year)")
+      .setPlaceholder("MM/DD/YYYY")
+      .setRequired(true)
+      .setStyle(TextInputStyle.Short);
+
+    const firstActionRow =
+      new ActionRowBuilder<ModalActionRowComponentBuilder>().addComponents(
+        ageInput
+      );
+
+    modal.addComponents(firstActionRow);
+
+    await interaction.showModal(modal);
+  },
+};
+
+export default verifyCommand;
