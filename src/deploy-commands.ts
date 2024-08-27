69d826563bcc18b471fb40c34b086fc8be8f1fdc
Aurorah Harmony <aurorahHarmony@gmail.com>
2024-08-28 01:19:04 +1200

    Fix command deployment

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -5,39 +5,20 @@ import {
   Routes,
 } from "discord.js";
 import { config } from "./config";
-import fs from "fs";
-import path from "path";
-import { Command } from "./types/Command";
+import { commands as cmds } from "./commands/index";
 
 const commands: RESTPostAPIChatInputApplicationCommandsJSONBody[] = [];
 
-const foldersPath = path.join(__dirname, "commands");
-const commandFolders = fs.readdirSync(foldersPath);
-
 (async () => {
-  for (const folder of commandFolders) {
-    const commandsPath = path.join(foldersPath, folder);
-    const commandFiles = fs
-      .readdirSync(commandsPath)
-      .filter((file) => file.endsWith(".ts") || file.endsWith(".js"));
+  for (const commandName in cmds) {
+    const command = cmds[commandName];
 
-    for (const file of commandFiles) {
-      const filePath = path.join(commandsPath, file);
-
-      try {
-        const commandModule = await import(filePath);
-        const command: Command = commandModule.default;
-
-        if ("data" in command && "execute" in command) {
-          commands.push(command.data.toJSON());
-        } else {
-          console.log(
-            `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
-          );
-        }
-      } catch (error) {
-        console.error(`Failed to load command ${filePath}: ${error}`);
-      }
+    if ("data" in command && "execute" in command) {
+      commands.push(command.data.toJSON());
+    } else {
+      console.log(
+        `[WARNING] The command at ${commandName} is missing a required "data" or "execute" property.`
+      );
     }
   }
 
@@ -50,9 +31,13 @@ const commandFolders = fs.readdirSync(foldersPath);
 
     // The put method is used to fully refresh all commands in the guild with the current set
     const data = (await rest.put(
-      Routes.applicationGuildCommands(
-        config.DISCORD_CLIENT_ID,
-        config.DEV_SERVER_ID
+      // Routes.applicationGuildCommands(
+      //   config.DISCORD_CLIENT_ID,
+      //   config.DEV_SERVER_ID
+      // ),
+      Routes.applicationCommands(
+        config.DISCORD_CLIENT_ID
+        // config.DEV_SERVER_ID
       ),
       { body: commands }
     )) as RESTPostAPIApplicationCommandsJSONBody[];
